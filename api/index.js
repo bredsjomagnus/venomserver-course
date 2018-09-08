f57c33de15a3435c45906834fbeb80290f40a017
Magnus Andersson <bredsjomagnus@yahoo.se>
2018-09-08 11:21:04 +0200

    Files

---
+++ api/index.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-alert */
+
+const express = require('express');
+const router = express.Router();
+
+require('./routes/transaction')(router);
+require('./routes/user')(router);
+
+module.exports = router;
